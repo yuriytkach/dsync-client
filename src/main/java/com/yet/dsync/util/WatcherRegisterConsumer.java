0d2012e5ba2f052aa97d5cf11caec56eaea16c11
overriderb <overriderb@gmail.com>
2016-12-02 17:16:53 +0200

    Issue #3: Use ExtendedWatchEventModifier.FILE_TREE for Windows OS to register watcher on a new directory.

--- src/main/java/com/yet/dsync/util/WatcherRegisterConsumer.java
+++ src/main/java/com/yet/dsync/util/WatcherRegisterConsumer.java
@@ -14,7 +14,9 @@ import java.util.function.Consumer;
 
 //import com.sun.nio.file.SensitivityWatchEventModifier;
 
+import com.sun.nio.file.ExtendedWatchEventModifier;
 import com.yet.dsync.exception.DSyncClientException;
+import org.apache.commons.lang.SystemUtils;
 
 public class WatcherRegisterConsumer implements Consumer<Path> {
 
@@ -27,31 +29,64 @@ public class WatcherRegisterConsumer implements Consumer<Path> {
     }
 
     @Override
-    public void accept(Path p) {
-        if (!p.toFile().exists() || !p.toFile().isDirectory()) {
-            throw new DSyncClientException("folder " + p + " does not exist or is not a directory");
+    public void accept(Path path) {
+        if (!path.toFile().exists() || !path.toFile().isDirectory()) {
+            throw new DSyncClientException("folder " + path + " does not exist or is not a directory");
         }
         try {
-            Files.walkFileTree(p, new SimpleFileVisitor<Path>() {
-                @Override
-                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                    System.out.println("registering " + dir + " in watcher service");
-                    WatchKey watchKey = dir
-                            .register(watchService,
-                                    new WatchEvent.Kind[] { StandardWatchEventKinds.ENTRY_CREATE,
-                                            StandardWatchEventKinds.ENTRY_DELETE,
-                                            StandardWatchEventKinds.ENTRY_MODIFY });
-                    // , SensitivityWatchEventModifier.HIGH);
-                    if (watchKeyConsumer != null) {
-                        watchKeyConsumer.accept(watchKey);
-                    }
-                    return FileVisitResult.CONTINUE;
-                }
-            });
+            if (SystemUtils.IS_OS_UNIX) {
+                registerWatchersRecursively(path);
+            } else if (SystemUtils.IS_OS_WINDOWS) {
+                registerWatcherForFileTree(path);
+            }
         } catch (IOException e) {
-            throw new DSyncClientException("Error registering path " + p);
+            throw new DSyncClientException("Error registering path " + path);
         }
 
+    }
+
+    /**
+     * Register watchers recursively. Applicable for unix type operation systems.
+     *
+     * @param path to directory
+     * @throws IOException during watchers registration
+     */
+    private void registerWatchersRecursively(Path path) throws IOException {
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.println("registering " + dir + " in watcher service");
+                WatchKey watchKey = dir
+                        .register(watchService,
+                                StandardWatchEventKinds.ENTRY_CREATE,
+                                StandardWatchEventKinds.ENTRY_DELETE,
+                                StandardWatchEventKinds.ENTRY_MODIFY);
+//                 , SensitivityWatchEventModifier.HIGH);
+                if (watchKeyConsumer != null) {
+                    watchKeyConsumer.accept(watchKey);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    /**
+     * Register watcher for a whole file tree using undocumented ExtendedWatchEventModifier.FILE_TREE.
+     * Applicable ONLY for Windows type operation systems.
+     *
+     * @param path to directory
+     * @throws IOException during watchers registration
+     */
+    private void registerWatcherForFileTree(Path path) throws IOException {
+        WatchKey watchKey = path.register(watchService,
+                new WatchEvent.Kind[] {
+                        StandardWatchEventKinds.ENTRY_CREATE,
+                        StandardWatchEventKinds.ENTRY_DELETE,
+                        StandardWatchEventKinds.ENTRY_MODIFY},
+                ExtendedWatchEventModifier.FILE_TREE);
+        if (watchKeyConsumer != null) {
+            watchKeyConsumer.accept(watchKey);
+        }
     }
 
 }
